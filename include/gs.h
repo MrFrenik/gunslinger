208e9c4c74e3bdb2e16dc34103aca4a342221aa6
John Jackson <mrfrenik@gmail.com>
2020-12-14 13:17:42 -0600

    Update gs.h

--- include/gs.h
+++ include/gs.h
@@ -15,8 +15,6 @@ extern "C" {
 #include "graphics/gs_camera.h"
 #include "graphics/gs_graphics.h"
 #include "graphics/gs_quad_batch.h"
-#include "graphics/gs_material.h"
-#include "graphics/gs_mesh.h"
 #include "audio/gs_audio.h"
 #include "lexer/gs_lexer.h"
 
