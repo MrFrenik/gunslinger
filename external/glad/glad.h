97db2a3038c9cf6165db28ba299eb0ffe498a148
jjackson4 <jjackson4@fossil.com>
2021-01-07 19:30:34 -0600

    fixed relative include paths

--- external/glad/glad.h
+++ external/glad/glad.h
@@ -86,7 +86,7 @@ GLAPI int gladLoadGL(void);
 
 GLAPI int gladLoadGLLoader(GLADloadproc);
 
-#include "external/KHR/khrplatform.h"
+#include "../KHR/khrplatform.h"
 typedef unsigned int GLenum;
 typedef unsigned char GLboolean;
 typedef unsigned int GLbitfield;
--- external/glad/glad_impl.h
+++ external/glad/glad_impl.h
@@ -22,7 +22,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "external/glad/glad.h"
+#include "glad.h"
 
 #ifdef GLAD_IMPL
 
