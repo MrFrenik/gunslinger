33736073cc30b3ec4abf39509453c9289a540d22
John Jackson <mrfrenik@gmail.com>
2021-01-20 17:28:34 -0600

    added meta utility

---
+++ util/gs_meta.h
@@ -0,0 +1,199 @@
+/*================================================================
+    * Copyright: 2020 John Jackson
+    * GSAsset: Meta Data Util for Gunslinger
+    * File: gs_meta.h
+    All Rights Reserved
+=================================================================*/
+
+#ifndef __GS_META_H__
+#define __GS_META_H__
+
+/*
+    USAGE: (IMPORTANT)
+
+    =================================================================================================================
+
+    Before including, define the gunslinger asset manager implementation like this:
+
+        #define GS_META_IMPL
+
+    in EXACTLY ONE C or C++ file that includes this header, BEFORE the
+    include, like this:
+
+        #define GS_META_IMPL
+        #include "gs_meta.h"
+
+    All other files should just #include "gs_meta.h" without the #define.
+
+    MUST include "gs.h" and declare GS_IMPL BEFORE this file, since this file relies on that:
+
+        #define GS_IMPL
+        #include "gs.h"
+
+        #define GS_META_IMPL
+        #include "gs_meta.h"
+
+    ================================================================================================================
+*/
+
+/*==== Interface ====*/
+
+typedef enum gs_meta_property_type
+{
+    GS_META_PROPERTY_TYPE_U8,
+    GS_META_PROPERTY_TYPE_U16,
+    GS_META_PROPERTY_TYPE_U32,
+    GS_META_PROPERTY_TYPE_U64,
+    GS_META_PROPERTY_TYPE_S8,
+    GS_META_PROPERTY_TYPE_S16,
+    GS_META_PROPERTY_TYPE_S32,
+    GS_META_PROPERTY_TYPE_S64,
+    GS_META_PROPERTY_TYPE_F32,
+    GS_META_PROPERTY_TYPE_F64
+} gs_meta_property_type;
+
+typedef struct gs_meta_property_t
+{
+    const char* name;           
+    uint32_t offset;            
+    gs_meta_property_type type;
+} gs_meta_property_t;
+
+typedef struct gs_meta_class_t
+{
+    gs_meta_property_t* properties;
+    uint32_t property_count;
+} gs_meta_class_t;
+
+typedef struct gs_meta_registry_t
+{
+    gs_hash_table(u64, gs_meta_class_t) meta_classes;
+    uint32_t last_id;
+} gs_meta_registry_t;
+
+typedef struct gs_meta_class_decl_t
+{
+    gs_meta_property_t* properties;
+    size_t properties_size;
+} gs_meta_class_decl_t;
+
+GS_API_DECL gs_meta_registry_t gs_meta_registry_new();
+
+#define gs_meta_register_class(META, T, DECL)\
+    do {\
+        uint32_t ct = (DECL)->properties_size / sizeof(gs_meta_property_t);\
+        size_t sz = sizeof(gs_meta_property_t) * ct;\
+        gs_meta_class_t cls = gs_default_val();\
+        cls.property_count = ct;\
+        cls.properties = gs_malloc(sz);\
+        memcpy(cls.properties, (DECL)->properties, sz);\
+        gs_hash_table_insert((META)->meta_classes, gs_hash_str64(gs_to_str(T)), cls);\
+    } while (0)
+
+#define gs_meta_get_class(META, T)\
+    gs_hash_table_getp((META)->meta_classes, gs_hash_str64(gs_to_str(T)))
+
+#define gs_meta_getv(OBJ, T, PROP)\
+    (*((T*)((uint8_t*)(OBJ) + (PROP)->offset)))
+
+#define gs_meta_getvp(OBJ, T, PROP)\
+    (((T*)((uint8_t*)(OBJ) + (PROP)->offset)))
+
+/*
+    #define GS_META_IMPL
+    #include <gs/util/gs_meta.h>
+
+    gs_meta_data_t* data = gs_meta_getp(&meta, gs_asset_texture_t);
+    gs_assert(data);
+
+    gs_asset_texture_t tex = {0};
+
+    typedef struct gs_meta_property_t
+    {
+        gs_meta_property_type type;
+        uint32_t offset;
+    } gs_meta_property_t;
+
+    typedef struct gs_meta_data_t
+    {
+        gs_dyn_array(gs_meta_property_t) properties;
+        uint32_t property_count;
+    } gs_meta_data_t;
+
+    // Have to feed in meta registry data...somehow
+    typedef struct gs_meta_registry_t
+    {
+    } gs_meta_registry_t;
+
+    gs_meta_registry_t meta = gs_meta_registry_new();
+
+    gs_meta_property_t properties[] = {
+        (gs_meta_property_t){.offset = gs_offset(T, prop)},
+        ...
+    };
+
+    gs_meta_registry_cls_decl_t cdecl = {
+        .properties = properties,
+        .size = sizeof(properties)
+    };
+
+    gs_meta_register_cls(&meta, T, &cdecl);
+
+    for (uint32_t i = 0; i < data->property_count; ++i)
+    {
+        gs_meta_property_t* prop = &data->properties[i];
+        switch (prop->type)
+        {
+            case GS_META_PROPERTY_TYPE_F32: 
+            {
+                f32 v = gs_meta_class_getv(&tex, f32, prop);
+            } break;
+
+        }
+    }
+
+    // Utility for generating reflection data / reading reflection data
+    // Has to be run as a pre-processing step
+    // Generate reflection information
+    // Write parser to generate reflection code
+
+    Types:
+
+    u8
+    i8
+    u16
+    i16
+    u32
+    i32
+    u64
+    i64
+    f32
+    f64
+    gs_dyn_array(u64)
+
+    Example: 
+    struct gs_asset_texture_t
+    {
+        f32 : mValue,
+        f64 : 
+        u32 : 
+        i32 : 
+    };
+    
+*/
+
+/*==== Implementation ====*/
+
+#ifdef GS_META_IMPL
+
+gs_meta_registry_t gs_meta_registry_new()
+{
+    gs_meta_registry_t meta = gs_default_val();
+    return meta;
+}
+
+#undef GS_META_IMPL
+
+#endif // GS_META_IMPL
+#endif // __GS_META_H__
+
