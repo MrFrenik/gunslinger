677ad4e1e6d704205a06a7aa854d26546d373fd5
jjackson4 <jjackson4@fossil.com>
2021-07-09 11:25:49 -0500

    ccd/physics
    - added modified ccd impl to external
    - fixed physics implementations for collision response/resolution

---
+++ external/ccd/libccd.c
@@ -0,0 +1,10 @@
+// libccd unity build file
+#include "src/ccd/ccd.h"
+#include "src/ccd/ccd_quat.h"
+#include "src/ccd/ccd_vec3.h"
+
+#include "src/ccd_vec3.c"
+#include "src/ccd_support.c"
+#include "src/ccd_polytope.c"
+#include "src/ccd_mpr.c"
+#include "src/ccd.c"
--- external/stb/stb_vorbis.c
+++ external/stb/stb_vorbis.c
@@ -5105,19 +5105,19 @@ stb_vorbis * stb_vorbis_open_memory(const unsigned char *data, int len, int *err
 #define PLAYBACK_LEFT     2
 #define PLAYBACK_RIGHT    4
 
-#define L  (PLAYBACK_LEFT  | PLAYBACK_MONO)
-#define C  (PLAYBACK_LEFT  | PLAYBACK_RIGHT | PLAYBACK_MONO)
-#define R  (PLAYBACK_RIGHT | PLAYBACK_MONO)
+#define STB_L  (PLAYBACK_LEFT  | PLAYBACK_MONO)
+#define STB_C  (PLAYBACK_LEFT  | PLAYBACK_RIGHT | PLAYBACK_MONO)
+#define STB_R  (PLAYBACK_RIGHT | PLAYBACK_MONO)
 
 static int8 channel_position[7][6] =
 {
    { 0 },
-   { C },
-   { L, R },
-   { L, C, R },
-   { L, R, L, R },
-   { L, C, R, L, R },
-   { L, C, R, L, R, C },
+   { STB_C },
+   { STB_L, STB_R },
+   { STB_L, STB_C, STB_R },
+   { STB_L, STB_R, STB_L, STB_R },
+   { STB_L, STB_C, STB_R, STB_L, STB_R },
+   { STB_L, STB_C, STB_R, STB_L, STB_R, STB_C },
 };
 
 
