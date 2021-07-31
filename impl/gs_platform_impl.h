4f8e401eca2a6db10a0ae4c4341b1d90d525e085
John Jackson <mrfrenik@gmail.com>
2021-07-30 20:55:48 -0500

    Update gs_platform_impl.h

--- impl/gs_platform_impl.h
+++ impl/gs_platform_impl.h
@@ -166,32 +166,6 @@ uint32_t gs_platform_uuid_hash(const gs_uuid_t* uuid)
 
 #endif
 
-#include <iostream>
-//Returns the last Win32 error, in string format. Returns an empty string if there is no error.
-std::string GetLastErrorAsString()
-{
-    //Get the error message ID, if any.
-    DWORD errorMessageID = ::GetLastError();
-    if(errorMessageID == 0) {
-        return std::string(); //No error message has been recorded
-    }
-
-    LPSTR messageBuffer = nullptr;
-
-    //Ask Win32 to give us the string version of that message ID.
-    //The parameters we pass in, tell Win32 to create the buffer that holds the message for us (because we don't yet know how long the message string will be).
-    size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-                                 NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
-
-    //Copy the error message into a std::string.
-    std::string message(messageBuffer, size);
-
-    //Free the Win32's string's buffer.
-    LocalFree(messageBuffer);
-
-    return message;
-}
-
 GS_API_DECL void gs_platform_dir_iter_init(gs_platform_dir_iter_t* iter, const char* path, bool32 recursive)
 {
 	memcpy(iter->path, path, GS_PLATFORM_DIR_MAX_STR_SZ);
@@ -205,9 +179,8 @@ GS_API_DECL void gs_platform_dir_iter_init(gs_platform_dir_iter_t* iter, const c
 		gs_platform_dir_data_t* hndl = (gs_platform_dir_data_t*)iter->hndl;
 		hndl->hfind = FindFirstFile(".\\assets\\*", &hndl->fdata);
 		if (hndl->hfind == INVALID_HANDLE_VALUE) 
-		{
-			std::string err = GetLastErrorAsString();
-			gs_println("error: gs_platform_dir_iter_create:FindFirstFile failed (%s)\n", err.c_str());
+                {
+			gs_println("error: gs_platform_dir_iter_create:FindFirstFile failed (%s)\n", GetLastError());
 			gs_free(iter->hndl);
 			iter->hndl = NULL;
 		} 
