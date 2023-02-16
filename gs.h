92379e6883c5fb7cac035ccd00c15dd6c5115c86
Samdal <samdal@protonmail.com>
2023-02-16 13:20:29 +0100

    fix auto resize for small textures

--- gs.h
+++ gs.h
@@ -7928,8 +7928,9 @@ bool gs_asset_font_load_from_memory(const void* memory, size_t sz, void* out, ui
     } 
 
     // Poor attempt at an auto resized texture
-    const uint32_t w = (point_size/32 * 512) + (point_size/32 * 512) % 512;
-    const uint32_t h = (point_size/32 * 512) + (point_size/32 * 512) % 512;
+    const uint32_t point_wh = gs_max(point_size, 32);
+    const uint32_t w = (point_wh/32 * 512) + (point_wh/32 * 512) % 512;
+    const uint32_t h = (point_wh/32 * 512) + (point_wh/32 * 512) % 512;
 
     const uint32_t num_comps = 4;
     u8* alpha_bitmap = (uint8_t*)gs_malloc(w * h);
