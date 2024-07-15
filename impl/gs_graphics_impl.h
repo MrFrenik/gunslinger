d2c7689ec89710b06b533ee4926b90ed7c1c845a
alec <aleckennedy4@gmail.com>
2024-07-15 14:24:06 -0400

    gsgl_message_cb failing to compile under c++ emscripten build

--- impl/gs_graphics_impl.h
+++ impl/gs_graphics_impl.h
@@ -205,15 +205,17 @@ void gsgl_pipeline_state()
     )
 }
 
-void GLAPIENTRY
+void// GLAPIENTRY (error: variable has incomplete type 'void')
 gsgl_message_cb(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, 
     const GLchar* message, void* user_param)
 {
     return;
+    /*
     if (type == GL_DEBUG_TYPE_ERROR) {
         gs_println("GL [DEBUG]: %s type = 0x%x, severity = 0x%x, message = %s", 
             type == GL_DEBUG_TYPE_ERROR ? "GL ERROR" : "", type, severity, message);
     }
+    */
 }
 
 /* GS/OGL Utilities */
