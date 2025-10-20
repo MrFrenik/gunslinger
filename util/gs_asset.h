b3b28c5b8afe3e5d94f0eb97aa0c61ecb3a7a22d
John Jackson <mrfrenik@gmail.com>
2025-10-20 10:19:27 -0500

    Updating license agreement

--- util/gs_asset.h
+++ util/gs_asset.h
@@ -1,9 +1,33 @@
-/*================================================================
-	* Copyright: 2020 John Jackson
+/*==============================================================================================================
+	* Copyright (c) 2020 John Jackson
 	* GSAsset: Asset Manager Util for Gunslinger
 	* File: gs_asset.h
-	All Rights Reserved
-=================================================================*/
+    * Github: https://github.com/MrFrenik/gunslinger
+
+    * All Rights Reserved
+
+    * MIT License
+
+    * May all those that this source may reach be blessed by the LORD and find peace and joy in life.
+
+    * Everyone who drinks of this water will be thirsty again; but whoever drinks of the water
+    * that I will give him shall never thirst; John 4:13-14
+
+    * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+    * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+    * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+    * and to permit persons to whom the Software is furnished to do so, subject to the following conditions: 
+
+    * The above copyright, blessing, biblical verse, notice and this permission notice shall be included in all 
+    * copies or substantial portions of the Software.
+
+    * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+    * TO THE WARRANTIES OF MECHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+    * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    * IN THE SOFTWARE.  
+    
+=================================================================================================================*/
 
 #ifndef GS_ASSET_H
 #define GS_ASSET_H
